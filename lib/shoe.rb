c9084dbcd2c8605a3580e36cce49af690d607f2a
James Ferguson <ncjamesferguson@gmail.com>
2017-12-09 21:41:53 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if !(BRANDS.include(brand))
+    if !(BRANDS.include?(brand))
     BRANDS << brand
     end
   end
@@ -18,7 +18,7 @@ class Shoe
 
   def brand=(brand)
     @brand = brand
-    if !(BRANDS.include(brand))
+    if !(BRANDS.include?(brand))
     BRANDS << brand
     end
   end
